2998e564aa85825a01cf9517ba789a5abebb0620
johnmurch <john@ipullrank.com>
2023-06-16 09:42:28 -0400

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+import zlib from "zlib";
+import axios from "axios";
+import fs from "fs";
+
+// Function to extract sitemap of sitemaps
+async function extractSitemapOfSitemaps(url) {
+  const response = await axios.get(url, { responseType: "arraybuffer" });
+  const buffer = Buffer.from(response.data);
+  let sitemapContent;
+
+  // Decompress gzip content
+  if (response.headers["content-encoding"] === "gzip") {
+    sitemapContent = zlib.gunzipSync(buffer).toString();
+  } else {
+    sitemapContent = buffer.toString();
+  }
+
+  // Regular expression to extract URLs from the sitemap
+  const urlRegex = /<loc>(.*?)<\/loc>/g;
+  let match;
+  const sitemaps = [];
+
+  // Extract URLs from the sitemap
+  while ((match = urlRegex.exec(sitemapContent))) {
+    const sitemapUrl = match[1];
+    sitemaps.push(sitemapUrl);
+  }
+
+  return sitemaps;
+}
+
+// Main function to process the sitemap of sitemaps
+async function processSitemapOfSitemaps(sitemapOfSitemapsUrl) {
+  const sitemapOfSitemaps = await extractSitemapOfSitemaps(
+    sitemapOfSitemapsUrl
+  );
+  const allUrls = [];
+
+  // Process each individual sitemap
+  for (const sitemapUrl of sitemapOfSitemaps) {
+    const sitemapUrls = await extractSitemapOfSitemaps(sitemapUrl);
+    allUrls.push(...sitemapUrls);
+  }
+
+  // Write URLs to a file
+  const csvContent = allUrls.join("\n");
+  fs.writeFileSync("urls.csv", csvContent);
+}
+
+// Call the main function with the URL of the sitemap of sitemaps
+const sitemapOfSitemapsUrl =
+  "https://www.target.com/sitemap_taxonomy-brand-index.xml.gz";
+processSitemapOfSitemaps(sitemapOfSitemapsUrl);
